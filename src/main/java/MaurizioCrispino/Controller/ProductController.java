6b1e912bf86e3b527c9480fb72b116e566f6d392
MaurizioCr <crispinomaurizio034@gmail.com>
2025-02-20 13:20:01 +0100

    ProductController

---
+++ src/main/java/MaurizioCrispino/Controller/ProductController.java
@@ -0,0 +1,17 @@
+package MaurizioCrispino.Controller;
+import MaurizioCrispino.Entities.Product;
+import MaurizioCrispino.Services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+@CrossOrigin(origins = "http://localhost:3000")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts()
