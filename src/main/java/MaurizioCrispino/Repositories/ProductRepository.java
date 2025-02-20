19829a6d9bfb86b491024950b1e964f7b53c3727
MaurizioCr <crispinomaurizio034@gmail.com>
2025-02-20 13:08:08 +0100

    ProductRepository

---
+++ src/main/java/MaurizioCrispino/Repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package MaurizioCrispino.Repositories;
+import MaurizioCrispino.Entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByCategory(String category);
+}
