2316b8a3a4750a59f9e61bd9d5303bf9db8876f2
MaurizioCr <crispinomaurizio034@gmail.com>
2025-02-26 17:03:35 +0100

    One to one

--- src/main/java/MaurizioCrispino/Entities/Cart.java
+++ src/main/java/MaurizioCrispino/Entities/Cart.java
@@ -1,36 +1,33 @@
 package MaurizioCrispino.Entities;
 
-import MaurizioCrispino.Repositories.UserRepository;
-import com.cloudinary.Cloudinary;
-import com.cloudinary.utils.ObjectUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.ArrayList;
+import java.util.List;
 
-import java.io.IOException;
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
 
 public class Cart {
 
-    @Service
-    public class UserService {
-        @Autowired
-        private UserRepository utenteRepository;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-        @Autowired
-        private Cloudinary cloudinaryUploader;
+    @OneToOne
+    @JsonIgnore
+    @JoinColumn(name = "utente_id")
+    private User user;
 
-        private PasswordEncoder bcrypt = new BCryptPasswordEncoder(11);
+    @OneToMany(mappedBy = "cart", cascade = CascadeType.ALL)
+    private List<CartItem> oggettoCarrellos = new ArrayList<>();
 
-        public Page<User> getUsers(int page, int size, String orderBy) {
-            if (size >= 100) size = 100;
-            Pageable pageable = PageRequest.of(page, size, Sort.by(orderBy));
-            return utenteRepository.findAll(pageable);
-        }
-    }
+    private int totalPrice;
+
 }
