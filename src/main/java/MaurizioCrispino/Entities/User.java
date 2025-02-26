0635da87948a42e3c14c4db0da510778cab53385
MaurizioCr <crispinomaurizio034@gmail.com>
2025-02-26 16:59:36 +0100

    User role

--- src/main/java/MaurizioCrispino/Entities/User.java
+++ src/main/java/MaurizioCrispino/Entities/User.java
@@ -1,22 +1,65 @@
 package MaurizioCrispino.Entities;
 
+import MaurizioCrispino.Enums.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
 @Entity
-@Table(name = "users")
-@Getter @Setter
-@NoArgsConstructor @AllArgsConstructor
-public class User {
+@Table(name = "utenti")
+@ToString
+@NoArgsConstructor
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Column(unique = true, nullable = false)
+    @Column(name = "id_utente")
+    private long id;
     private String username;
-
-    @Column(nullable = false)
+    @Column(unique = true)
+    private String email;
     private String password;
+    private String nome;
+    private String cognome;
+    private String avatar;
+    @Enumerated(EnumType.STRING)
+    private Role role;
 
-    private String role; // "ADMIN" o "USER"
+    @OneToMany(mappedBy = "utente")
+    @JsonIgnore
+
+    @OneToOne(mappedBy = "user")
+    private Cart cart;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
