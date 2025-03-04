f56191638a2fd52c6c57cd5908313b31c422108a
MaurizioCr <crispinomaurizio034@gmail.com>
2025-03-04 18:55:38 +0100

    UnauthorizedException

---
+++ src/main/java/MaurizioCrispino/Exceptions/UnauthorizedException.java
@@ -0,0 +1,8 @@
+package MaurizioCrispino.Exceptions;
+
+public class UnauthorizedException extends RuntimeException{
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
+
