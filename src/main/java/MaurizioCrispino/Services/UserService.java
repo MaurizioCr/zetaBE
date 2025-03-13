73acbf9a45555674c522c215cc1e1f095ca29bf9
MaurizioCr <crispinomaurizio034@gmail.com>
2025-03-13 23:41:47 +0100

    findById

--- src/main/java/MaurizioCrispino/Services/UserService.java
+++ src/main/java/MaurizioCrispino/Services/UserService.java
@@ -16,4 +16,27 @@ public class UserService {
     public User findByUsername(String username) {
         return userRepository.findByUsername(username).orElse(null);
     }
+
+    public User findbyIdAndUpdate(long id, User body) {
+        User found = this.findById(id);
+        if (body.getCognome() != null && !body.getCognome().isEmpty()) {
+            found.setCognome(body.getCognome());
+        }
+        if (body.getNome() != null && !body.getNome().isEmpty()) {
+            found.setNome(body.getNome());
+        }
+        if (body.getEmail() != null && !body.getEmail().isEmpty()) {
+            found.setEmail(body.getEmail());
+        }
+        if (body.getUsername() != null && !body.getUsername().isEmpty()) {
+            found.setUsername(body.getUsername());
+        }
+
+        if (body.getAvatar() != null && !body.getAvatar().isEmpty()) {
+            found.setAvatar(body.getAvatar());
+        }
+
+        // Salva le modifiche nell'utente nel database
+        return userRepository.save(found);
+    }
 }
